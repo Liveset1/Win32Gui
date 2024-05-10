9bfafc2f53a5b2ed60f17c44eb95ead7fcd5c85a
Yousif William <yousifwilliam0123@gmail.com>
2024-05-10 18:51:26 -0400

    Initial

--- include/Win32Gui.h
+++ include/Win32Gui.h
@@ -1,24 +1,9 @@
 #ifndef WIN32GUI_H
 #define WIN32GUI_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #ifdef _WIN32
 
 #include <windows.h>
-#ifdef WIN32GUI_DLL
-    #ifdef WIN32GUI_BUILD_DLL
-        #define WIN32GUI_API __declspec(dllexport)
-    #else
-        #define WIN32GUI_API __declspec(dllimport)
-    #endif
-#endif
-
-#ifndef WIN32GUI_API
-    #define WIN32GUI_API
-#endif
 
 // =============================================================================
 // Window
@@ -34,27 +19,23 @@ typedef struct {
 
 // Initialization
 
-WIN32GUI_API Win32AppData *initialize_app_data(const char *class_name, Win32Size size, DWORD styles);
-WIN32GUI_API Win32Window *initialize_window(const char *window_title, Win32AppData *appData, HWND parentWindowHandle);
+Win32AppData *initialize_app_data(const char *class_name, Win32Size size, DWORD styles);
+Win32Window *initialize_window(const char *window_title, Win32AppData *appData, HWND parentWindowHandle);
 
 // Deconstruction
 
-WIN32GUI_API void destroy_win32_window(Win32Window *window);
+void destroy_win32_window(Win32Window *window);
 
 // Configurations
 
-WIN32GUI_API void set_window_visibility(Win32Window *window, int visible);
-WIN32GUI_API void set_window_title(Win32Window *window,  const char *window_title);
+void set_window_visibility(Win32Window *window, int visible);
+void set_window_title(Win32Window *window,  const char *window_title);
 
 // Events
-WIN32GUI_API int is_window_running(Win32Window *window);
-WIN32GUI_API void window_poll_events();
+int is_window_running(Win32Window *window);
+void window_poll_events();
 
 // =============================================================================
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
 #endif
